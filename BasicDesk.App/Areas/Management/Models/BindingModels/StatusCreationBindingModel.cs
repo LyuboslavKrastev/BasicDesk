0e92f4744f7d3390c75ac494e2e303f2ceca6450
Lyuboslav Krastev <luboslav.krustev@abv.bg>
2018-08-25 23:29:42 +0300

    Status Updates

---
+++ BasicDesk.App/Areas/Management/Models/BindingModels/StatusCreationBindingModel.cs
@@ -0,0 +1,14 @@
+﻿using BasicDesk.Common.Constants.Validation;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BasicDesk.App.Areas.Management.Models.BindingModels
+{
+    public class StatusCreationBindingModel
+    {
+        [Required]
+        [MinLength(RequestStatusConstants.NameMinLength)]
+        [MaxLength(RequestStatusConstants.NameMaxLength)]
+        public string Name { get; set; }
+    }
+}
---
+++ BasicDesk.App/Areas/Management/Pages/Statuses/Create.cshtml.cs
@@ -0,0 +1,58 @@
+﻿using BasicDesk.App.Areas.Management.Pages;
+using BasicDesk.App.Common;
+using BasicDesk.App.Helpers.Messages;
+using BasicDesk.Common.Constants;
+using BasicDesk.Common.Constants.Validation;
+using BasicDesk.Data;
+using BasicDesk.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace BasicDesk.App.Areas.Management.Pages.Statuses
+{
+    [Authorize(Roles = WebConstants.AdminRole)]
+    public class CreateModel : PageModel
+    {
+        private BasicDeskDbContext dbContext;
+
+        [BindProperty]
+        [Required]
+        [MinLength(RequestStatusConstants.NameMinLength)]
+        [MaxLength(RequestStatusConstants.NameMaxLength)]
+        public string StatusName {get; set; }
+
+        public CreateModel(BasicDeskDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var status = new RequestStatus
+            {
+                Name = StatusName
+            };
+
+            await this.dbContext.RequestStatuses.AddAsync(status);
+
+            this.dbContext.SaveChanges();
+
+            this.TempData.Put("__Message", new MessageModel()
+            {
+                Type = MessageType.Success,
+                Message = "Status created successfully"
+            });
+
+
+            return RedirectToPage("/Index");
+        }     
+    }
+}
